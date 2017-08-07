66e5cb68dd709143d20624955cd0fc93a271a759
Vlad Fang <vlad.fang@711media.de>
2017-08-07 19:55:42 +0300

    Added tutorial based app

---
+++ EasyTravel/index.ios.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+import App from './src/app';
+
+// AppRegistry.registerComponent('MovieTickets', () => App);
+
+AppRegistry.registerComponent('EasyTravel', () => App);
---
+++ EasyTravel/src/app.js
@@ -0,0 +1,10 @@
+import { StackNavigator } from 'react-navigation';
+import Movies from './Movies';
+import Confirmation from './Confirmation';
+
+const RouteApp = StackNavigator({
+    Movies: { screen : Movies },
+    Confirmation: { screen: Confirmation }
+});
+
+export default RouteApp;
